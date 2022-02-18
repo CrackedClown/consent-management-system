f1e38d4e195cb2be5f86af06a6b53d78f16ed85f
ConsultingCreep <harshitagrawal0397@gmail.com>
2022-02-19 00:07:27 +0530

    Initial commit

---
+++ cm/src/test/java/com/cms/cm/CmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ healthcare/src/test/java/com/cms/portal/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.portal.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
