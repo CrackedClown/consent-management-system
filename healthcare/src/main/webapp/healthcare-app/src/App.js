815b465a4423ee43c2ca2cb241cfd2d87e991e1f
Taksh Patel <tkpatel8055@gmail.com>
2022-04-28 00:37:51 +0530

    created Navbar

--- healthcare/src/main/webapp/healthcare-app/src/App.js
+++ healthcare/src/main/webapp/healthcare-app/src/App.js
@@ -11,19 +11,20 @@ import CreateConsent from './CreateConsent';
 import ViewConsents from './ViewConsents';
 import ViewEHR from './ViewEHR';
 import DelegateConsent from './DelegateConsent';
+import Login from './Login';
 function App() {
     return (
       <>
         <Router>
           <Routes>
-            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/" element={<Login/>} />
+            <Route path="/home" element={<Home/>} />
             <Route path="/addPatientEHR" element={<AddPatientEHR/>} />
             <Route path="/patientRegistration" element={<PatientRegistration/>} />
             <Route path="/createConsent" element={<CreateConsent/>} />
             <Route path="/viewConsents" element={<ViewConsents/>} />
             <Route path="/viewConsents/viewEHR" element={<ViewEHR/>} />
             <Route path="/viewConsents/delegateConsent" element={<DelegateConsent/>} />
-            <Route path="/" element={<Navigate to="/"/>}/>
           </Routes>
         </Router>
       </>
--- healthcare/src/main/webapp/healthcare-app/src/Home.js
+++ healthcare/src/main/webapp/healthcare-app/src/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-  
+import Navbar from "./Navbar"
 const Home = () => {
   return (
     <div>
+      <Navbar/>
       <h1>Home Page</h1>
       <br />
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/home">Home</Link>
         </li>
         <li>
           <Link to="/patientRegistration">Patient Registration</Link>
